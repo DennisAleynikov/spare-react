591afa2166f2ab86a47291e1ee0c8a9ec0e9d0e3
Dennis Aleynikov <daleynikov@gmail.com>
2016-03-11 23:37:15 -0800

    add item reducer

--- src/reducers/item/itemReducer.js
+++ src/reducers/item/itemReducer.js
@@ -19,8 +19,31 @@ const formValidation = require('./profileFormValidation').default;
  *
  */
 const {
-  ON_PROFILE_FORM_FIELD_CHANGE,
-  GET_PROFILE_REQUEST,
-  GET_PROFILE_SUCCESS,
-  GET_PROFILE_FAILURE
+  ADD_TODO,
+  TOGGLE_TODO,
+  SET_VISIBILITY_FILTER
 } = require('../../lib/constants').default;
+
+const item = (state, action) => {
+  switch (action.type) {
+     case ‘ADD_TODO’:
+      return […state,{id: action.id, text: action.text,
+              completed:false}]
+
+     case ‘TOGGLE_TODO’:
+        return state.map(todo =>
+                if (todo.id !== action.id) {
+                  return todo
+                }
+                 return Object.assign({},
+                    todo, {completed: !todo.completed})
+            )
+
+      case ‘SET_VISIBILITY_FILTER’: {
+       return action.filter
+      }
+
+     default:
+      return state
+    }
+}
