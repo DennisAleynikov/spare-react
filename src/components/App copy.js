bdf2a5c33c8d11019d891e2250903b4c46c80e24
Dennis Aleynikov <daleynikov@gmail.com>
2016-03-14 23:48:17 -0700

    backup old app.js

---
+++ src/components/App copy.js
@@ -0,0 +1,14 @@
+import React from 'react'
+// import Footer from './Footer'
+import AddItem from '../containers/AddItem'
+import VisibleItemFeed from '../containers/VisibleItemFeed'
+
+const App = () => (
+  <div>
+    <AddItem />
+    <VisibleItemFeed />
+    // <Footer />
+  </div>
+)
+
+export default App
